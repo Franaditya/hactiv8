8a7e03232ab6227809b4e6aa942aa911cd6f2604
franaditya <franaditya@gmail.com>
2018-10-05 21:14:49 +0700

    add

--- Phase 0/Day 12/#3 ForEachModus.js
+++ Phase 0/Day 12/#3 ForEachModus.js
@@ -3,11 +3,11 @@ let terbanyak = [];
 
 function cariModus(arr) {
 	arr.sort(function(a, b) { return a > b });
-	//console.log(arr);
+	console.log(arr);
 	arr.forEach((item, index) => {
 		if(arr[index] == arr[index + 1]){
 			terbanyak.push(item);
-			//console.log(terbanyak);
+			console.log(terbanyak);
 			modus = index;
 			//console.log(modus)
 		}
@@ -19,8 +19,8 @@ function cariModus(arr) {
 	}
 
 }
-console.log(cariModus([ ])); 
-console.log(cariModus([2,2,2,5])); //2
-console.log(cariModus([10,2,7,5,2,5,5,5])); //5 
-console.log(cariModus([ 10, 8, 1, 9, 1, 7, 29, 1, 0, 4, 1, 7 ])); //1
+//console.log(cariModus([ ])); 
+//console.log(cariModus([2,2,2,5])); //2
+//console.log(cariModus([10,2,7,5,2,5,5,5])); //5 
+//console.log(cariModus([ 10, 8, 1, 9, 1, 7, 29, 1, 0, 4, 1, 7 ])); //1
 console.log(cariModus([10,10,3,5,2,10,2,12,12])); //10
